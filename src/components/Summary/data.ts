abe9b2a8464a5e50e56514ab61e29e71256e57eb
Erica Esteves <ericaesteves95@gmail.com>
2026-01-13 16:28:22 -0300

    Refatoração do JSON para uma lista mais genérica dividida por tópicos

--- src/components/Summary/data.ts
+++ src/components/Summary/data.ts
@@ -1,125 +1,167 @@
 export const resume = [
   {
+    type: "graduation",
+    title: "Faculdade Descomplica",
+    icon: ["fas", "graduation-cap"],
     date: "2023 - Atual",
-    institution: "Faculdade Descomplica",
-    location: "Full EAD",
-    course: "Tecnólogo em Análise e Desenvolvimento de Sistemas",
+    course: "Análise e Desenvolvimento de Sistemas",
+    formationType: "Tecnólogo",
     description: `Curso Superior focado em prática para o mercado, abordando conceitos como
         Lógica de Programação e Frameworks, Arquitetura de Software, Banco de Dados, 
         Design Thinking com metodologias Ágil, Scrum e Kanban, etc`,
-    hasIcon: true,
-    icon: ["fas", "graduation-cap"],
   },
   {
-    date: "2024 - 2024",
-    institution: "Udemy",
-    location: "Online",
-    course: "Curso Vue JS 2 - O Guia Completo (incl. Vue Router & Vuex)",
-    description: `Curso prático com diversos projetos introduzindo o framework
-        VueJS incluindo suas bibliotecas e ferramentas como Vuetify, Vuex, Router Vue, Vue CLI`,
-    hasIcon: false,
+    type: "course",
+    title: "Java & Spring",
+    icon: ["fab", "java"],
+    description: `Desenvolvimento de aplicações robustas no ecossistema Java. Experiência na criação de APIs RESTful escaláveis com Spring Boot, 
+    processamento de dados em lote com Spring Batch, comunicação em tempo real via WebSockets e integração com bancos de dados relacionais e NoSQL (Redis/MongoDB).`,
+    courseList: [
+      {
+        name: "API REST do 0 à AWS c. Spring Boot framework Java e Docker",
+        ch: 32,
+        certificationLink:
+          "https://udemy-certificate.s3.amazonaws.com/pdf/UC-7cf19cc4-ce5a-411e-8617-a44ea6ee42d6.pdf",
+      },
+      {
+        name: "Comunicação em Tempo Real com Spring Boot, WebSocket e Redis",
+        ch: 7,
+        certificationLink:
+          "https://www.udemy.com/certificate/UC-074a6c10-1d6c-4443-adff-ab277e692a62/",
+      },
+      {
+        name: "Desenvolvimento de jobs com Spring Batch",
+        ch: 7,
+        certificationLink:
+          "https://udemy-certificate.s3.amazonaws.com/pdf/UC-03dba346-fa84-47e5-831b-1faad33b2b29.pdf",
+      },
+      {
+        name: "Java - Programação Orientada a Objetos",
+        ch: 54.5,
+        certificationLink:
+          "https://www.udemy.com/certificate/UC-29bd9537-26ec-47d4-a94e-dc05fe069523/",
+      },
+      {
+        name: "NLW Connect - Aplicação backend Java com Maven, Spring boot e API Rest",
+        ch: 5,
+        certificationLink:
+          "https://app.rocketseat.com.br/certificates/c2f97486-89c3-4bd9-ae10-f16f2ad77e4d",
+      },
+    ],
   },
   {
-    date: "2025 - 2025",
-    institution: "Udemy",
-    location: "Online",
-    course: "Vue JS 3 Completo com Composition API, Vuex & Vue Router",
-    description: `Curso prático da versão 3 do VueJS com foco em composition api, gerenciamento de estado
-    com Vuex e direcionamento de rotas com Vue Router`,
-    hasIcon: false,
+    type: "course",
+    title: "Dart & Flutter",
+    icon: ["fab", "flutter"],
+    description: `Criação de aplicativos nativos multiplataforma (iOS e Android) com Flutter. 
+    Implementação de funcionalidades avançadas como geolocalização (Google Maps), uso de hardware (câmera), autenticação e design responsivo.`,
+    courseList: [
+      {
+        name: "Aprenda Flutter e Desenvolva Apps Para Android e IOS",
+        ch: 60,
+        certificationLink:
+          "https://www.udemy.com/certificate/UC-884acb58-ae71-4d78-a12b-c3b85efd17f0/",
+      },
+    ],
   },
   {
-    date: "2024 - 2024",
-    institution: "Udemy",
-    location: "Online",
-    course: "API REST do 0 c. Spring Boot framework",
-    description: `Curso prático de API Restful do zero utilizando: Swagger | JWT | JUnit 5 | Mockito | Docker | React JS`,
-    hasIcon: false,
+    type: "course",
+    title: "Vue JS",
+    icon: ["fab", "vuejs"],
+    description: `Desenvolvimento de interfaces reativas utilizando Vue.js 2 e 3. 
+    Domínio de Composition API, gerenciamento de estado complexo com Vuex, navegação com Vue Router e construção de interfaces modernas com Vuetify.`,
+    courseList: [
+      {
+        name: "Vue JS 3 Completo com Composition API, Vuex & Vue Router",
+        ch: 8.5,
+        certificationLink:
+          "https://udemy-certificate.s3.amazonaws.com/pdf/UC-0d328052-b8e3-4cf8-986d-0f9eb9a27746.pdf",
+      },
+      {
+        name: "Curso Vue JS 2 - O Guia Completo",
+        ch: 43,
+        certificationLink:
+          "https://www.udemy.com/certificate/UC-e2145e94-eb0d-4b7d-9d9e-3e97c6c79695/",
+      },
+    ],
   },
   {
-    date: "2025 - 2025",
-    institution: "Udemy",
-    location: "Online",
-    course: "Aprenda Flutter e Desenvolva Apps Para Android e IOS",
-    description: `Curso prático completo para aprender Google Flutter e Dart para a construção de apps nativas para iOS e Android
-    com funcionalidades como o Google Maps, a câmera do dispositivo, autenticação, com projetos práticos usando interfaces responsivas e adaptativas.`,
-    hasIcon: false,
+    type: "course",
+    title: "NodeJs & React",
+    subtitle: "TailwindCSS + TypeScript + ReactNative",
+    icon: ["fab", "react"],
+    description: `Desenvolvimento Full Stack moderno utilizando TypeScript. 
+    Criação de servidores eficientes com Node.js e interfaces dinâmicas com React e React Native para aplicações web e mobile integradas.`,
+    courseList: [
+      {
+        name: "Projeto Completo NodeJS, React, React Native, TypeScript",
+        ch: 23.5,
+        certificationLink:
+          "https://www.udemy.com/certificate/UC-c0d7c954-2fdc-4b5d-9360-46cd6ffc5d42/",
+      },
+      {
+        name: "Formação DEV - Projeto Convit3 Digital",
+        ch: 19.3,
+        certificationLink:
+          "https://escola.formacao.dev/certificado/6090df45-b8db-4c87-9a38-6516f05dbc12",
+      },
+      {
+        name: "Formação DEV - Projeto Convit3 Digital",
+        ch: 19.3,
+        certificationLink:
+          "https://escola.formacao.dev/certificado/6090df45-b8db-4c87-9a38-6516f05dbc12",
+      },
+      {
+        name: "Formação DEV - G3mer Store",
+        ch: 6.39,
+        certificationLink:
+          "https://escola.formacao.dev/certificado/e80ce81c-a3e6-4ada-9e40-f758c99e91df",
+      },
+    ],
   },
   {
-    date: "2024 - 2024",
-    institution: "Udemy",
-    location: "Online",
-    course: "Projeto Completo NodeJS, React, React Native, TypeScript",
-    description: `Curso focado em prática com um projeto Full Stack em NodeJs, ReactJS e React Native, 
-        feito projetos web e mobile de um sistema de gerenciamento de pedidos em uma pizzaria`,
-    hasIcon: false,
+    type: "course",
+    title: "HTML & CSS",
+    icon: ["fab", "html5"],
+    description: `Fundamentos sólidos de desenvolvimento web front-end. 
+    Foco em layouts responsivos, arquitetura de informação, usabilidade e princípios de UX Design para criação de interfaces acessíveis.`,
+    courseList: [
+      {
+        name: "Curso livre Web Design (HTML/CSS)",
+        ch: 160,
+        certificationLink:
+          "https://www.linkedin.com/in/erica-esteves-de-freitas/overlay/1635484570933/single-media-viewer/?profileId=ACoAACXydOgBQ4C74HEbRKJYyv4E8X_lbVpFKeg",
+      },
+    ],
   },
   {
-    date: "2023 - 2023",
-    institution: "Udemy",
-    location: "Online",
-    course: "Java - Programação Orientada a Objetos",
-    description: `Temas abordados no curso com foco em prática:
-        Classes, atributos e métodos, encapsulamento
-        Arrays e coleções, Herança, polimorfismo, interfaces, 
-        Programação funcional e expressões lambda, Stream API
-        Manipulação de arquivos, JPA, JDBC, Spring Boot e mais`,
-    hasIcon: false,
+    type: "course",
+    title: "Cloud & Infrastructure",
+    icon: ["fab", "aws"],
+    description: `Conhecimentos em arquitetura de nuvem AWS, cobrindo computação escalável, armazenamento de dados, segurança e modelos de rede para deploy de aplicações modernas.`,
+    courseList: [
+      {
+        name: "AWS para Iniciantes - Aprenda e Domine a nuvem Amazon",
+        ch: 7,
+        certificationLink:
+          "https://udemy-certificate.s3.amazonaws.com/pdf/UC-6abca541-b264-4108-a2c8-939b13b917bc.pdf",
+      },
+    ],
   },
   {
-    date: "2025 - 2025",
-    institution: "Udemy",
-    location: "Online",
-    course: "Comunicação em Tempo Real com Spring Boot, WebSocket e Redis",
-    description: `WebSocket implementado em um mini projeto de chat
-     com Java e SpringBoot no backend e Javascript no frontend.
-    Utilizando localmente o Docker para subir, por exemplo, Redis e MongoDB.`,
-    hasIcon: false,
-  },
-  {
-    date: "2024 - 2024",
-    institution: "Udemy",
-    location: "Online",
-    course: "Desenvolvimento de jobs com Spring Batch",
-    description: `Curso introdutório para criação de Jobs usando Spring Batch, na linguagem Java.
-    Jobs utilizando arquivos, informações de leitura e escrita em banco e serviços como envio automático de email.
-    `,
-    hasIcon: false,
-  },
-  {
-    date: "2022 - 2023",
-    institution: "Udemy",
-    location: "Online",
-    course: "Algoritmos e Lógica de Programação",
-    description: `Temas abordados no curso:
-        Variáveis, Entrada e saída de dados, Comando de atribuição (processamento),
-        Estrutura condicional, Estruturas repetitivas, Vetores
-        Matrizes, Testes de mesa, Depuração de programas.
-        Breve introdução às linguagens: Java, C, C++, C# e Python`,
-    hasIcon: false,
-  },
-  {
-    date: "2025 - 2025",
-    institution: "Udemy",
-    location: "Online",
-    course: "AWS para Iniciantes - Aprenda e Domine a nuvem Amazon",
-    description: `Temas abordados no curso:\n
-        conceitos fundamentais de Cloud Computing, 
-        serviços de Computação, Armazenamento e Banco de Dados, 
-        como a AWS esta estruturada e como é seu modelo de segurança
-        e escalabilidade`,
-    hasIcon: false,
-  },
-  {
-    date: "2019 - 2019",
-    institution: "Senac",
-    location: "Niterói, RJ",
-    course: "Curso livre Web Design",
-    description: `Curso de capacitação incial focado em Web Design - HTML e CSS. 
-        Ao longo do curso, os alunos aprendem a definir o escopo do site a partir de um briefing
-         e desenvolver um layout de interface responsiva, 
-         utilizando princípios de usabilidade, arquitetura da informação e UX.`,
-    hasIcon: false,
+    type: "course",
+    title: "Fundamentals & Logic",
+    icon: ["far", "file-code"],
+    description: `Base sólida em Ciência da Computação, incluindo lógica de programação, 
+    estruturas de dados (vetores/matrizes), algoritmos e introdução a múltiplas linguagens (C, C++, C#, Python e Java).`,
+    courseList: [
+      {
+        name: "Algoritmos e Lógica de Programação",
+        ch: 30.5,
+        certificationLink:
+          "https://www.udemy.com/certificate/UC-6d123797-6cff-4714-9191-64a71f614763/",
+      },
+    ],
   },
 ];
 
