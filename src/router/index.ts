3df634bb37f0538dec4c106344a4c2bc9df7d7fa
Erica Esteves <ericaesteves95@gmail.com>
2026-07-07 12:42:12 -0300

    mudando o roteamento para usar hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,40 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
-import Home from '@/components/Home/HomeIndex.vue'
-import Summary from '@/components/Summary/SummaryIndex.vue'
-import Projects from '@/components/Projects/ProjectsVue.vue'
-import Contact from '@/components/Contact/ContactVue.vue'
-
+import Home from "@/components/Home/HomeIndex.vue";
+import Summary from "@/components/Summary/SummaryIndex.vue";
+import Projects from "@/components/Projects/ProjectsVue.vue";
+import Contact from "@/components/Contact/ContactVue.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: Home,
-    meta: { title: "Erica's Portfolio" }
-  }, {
-    path: '/summary',
-    name: 'summary',
+    meta: { title: "Erica's Portfolio" },
+  },
+  {
+    path: "/summary",
+    name: "summary",
     component: Summary,
-    meta: { title: 'Resumo' }
-  }, {
-    path: '/projects',
-    name: 'projects',
+    meta: { title: "Resumo" },
+  },
+  {
+    path: "/projects",
+    name: "projects",
     component: Projects,
-    meta: { title: 'Projetos' }
-  }, {
-    path: '/contact',
-    name: 'contact',
+    meta: { title: "Projetos" },
+  },
+  {
+    path: "/contact",
+    name: "contact",
     component: Contact,
-    meta: { title: 'Contato' }
-  }
-]
+    meta: { title: "Contato" },
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
